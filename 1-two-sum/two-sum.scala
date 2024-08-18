7d211f0a24da13d201c09b88eaa09df4e92c2021
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-17 17:10:29 -0700

    Update two-sum.scala

--- 1-two-sum/two-sum.scala
+++ 1-two-sum/two-sum.scala
@@ -1,8 +1,8 @@
 object Solution {
   def twoSum(nums: Array[Int], target: Int): Array[Int] =
     def findPair(index: Int, current: Map[Int, Int]): Array[Int] =
-        val complementIndex = target - nums(index)
-        current.get(complementIndex) match 
+        val complement = target - nums(index)
+        current.get(complement) match 
             case Some(savedIndex) => Array(savedIndex, index)
             case None => findPair(index + 1, current + (nums(index) -> index))
 
