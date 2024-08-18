35620a290069cb2f2476c919ecdbc2f4eeb4dfcb
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-17 17:10:02 -0700

    Time: 66 ms (63.33%) | Memory: 52.9 MB (35.06%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+    const hashMap = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]
+        if (hashMap.has(complement)) {
+            return [hashMap.get(complement), i]
+        } else {
+            hashMap.set(nums[i], i)
+        }
+    }
+
+    return []
+};
