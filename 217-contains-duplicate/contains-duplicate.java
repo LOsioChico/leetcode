402ff0cae4719578f3006c59d349294cef4773a2
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-20 17:07:14 -0700

    Time: 7 ms (99.09%) | Memory: 60.7 MB (12.10%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> hashSet = new HashSet<>();
+
+    for(int num : nums){
+      if (!hashSet.add(num)){
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
