b31a3883a04b152271c9b177e4c11c4f68b57730
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-20 21:07:52 -0700

    Time: 915 ms (100.00%) | Memory: 61.1 MB (66.67%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.scala
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.scala
@@ -2,8 +2,8 @@ object Solution {
     def rowAndMaximumOnes(mat: Array[Array[Int]]): Array[Int] = {
         def iter(matrix: Array[Array[Int]], index: Int, result: Array[Int]): Array[Int] =
             matrix.headOption match
-                case Some(column) =>
-                    val rowSum = column.sum
+                case Some(row) =>
+                    val rowSum = row.sum
                     val updatedResult = if rowSum > result(1) then Array(index, rowSum) else result
                     iter(matrix.tail, index + 1, updatedResult)
                 case None         => result
