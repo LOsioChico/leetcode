500e562b6b677cc6eb4776dd2e0036dc0779a0f1
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-23 01:49:28 -0700

    Time: 653 ms (67.92%) | Memory: 57.3 MB (92.45%) - LeetSync

--- 71-simplify-path/simplify-path.scala
+++ 71-simplify-path/simplify-path.scala
@@ -2,13 +2,12 @@ object Solution {
     def simplifyPath(path: String): String = {
         def iter(path: Array[String], result: Array[String]): String =
             path.headOption match
-                case Some(string) if string == ".." =>
+                case Some(string) if string == ".."                     =>
                     if result.length > 0 then iter(path.tail, result.init)
                     else iter(path.tail, result)
-                case Some(string) =>
-                    if string.length > 0 && string != "." then iter(path.tail, result :+ s"/$string")
-                    else iter(path.tail, result)
-                case _                              =>
+                case Some(string) if string.length > 0 && string != "." => iter(path.tail, result :+ s"/$string")
+                case Some(string)                                       => iter(path.tail, result)
+                case None                                               =>
                     val resultString = result.mkString
                     if resultString.length > 0 then resultString else "/"
 
